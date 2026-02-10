edcdec963c835420c0aea6692095792588ad0201
Fran314 <francesco.ghog@gmail.com>
2026-02-10 13:06:40 +0100

    added noir from tarball because it's faster. Also left compiling from source if other people need it

--- playground/noir/flake.nix
+++ playground/noir/flake.nix
@@ -3,85 +3,105 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    rust-overlay.url = "github:oxalica/rust-overlay";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
     {
       self,
       nixpkgs,
-      rust-overlay,
-      flake-utils,
       ...
     }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs {
-          inherit system overlays;
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+
+      lib = pkgs.lib;
+
+      version = "1.0.0-beta.18";
+
+      # --- Precompiled tarball ---
+      nargo-tarball = pkgs.stdenv.mkDerivation {
+        pname = "nargo";
+        inherit version;
+
+        src = pkgs.fetchurl {
+          url = "https://github.com/noir-lang/noir/releases/download/v${version}/noir-x86_64-unknown-linux-gnu.tar.gz";
+          hash = "sha256-J6el8m5OKp+TC02EphaYHVm11IEQWP3kjfJfrLW3Tr4=";
         };
 
-        rustToolchain = pkgs.rust-bin.stable.latest.default;
+        sourceRoot = ".";
 
-        # Use the overlay's toolchain so we satisfy Noir's MSRV (1.89.0)
-        rustPlatform = pkgs.makeRustPlatform {
-          cargo = rustToolchain;
-          rustc = rustToolchain;
+        # autoPatchelfHook rewrites the ELF interpreter and RPATH so
+        # these precompiled binaries find glibc & friends in the Nix store.
+        nativeBuildInputs = [ pkgs.autoPatchelfHook ];
+
+        # Libraries the binaries are linked against (use `ldd` to check)
+        buildInputs = [
+          pkgs.stdenv.cc.cc.lib # libstdc++, libgcc_s
+          pkgs.zlib
+        ];
+
+        installPhase = ''
+          mkdir -p $out/bin
+          for bin in nargo noir-profiler noir-inspector; do
+            [ -f "$bin" ] && install -m755 "$bin" "$out/bin/"
+          done
+        '';
+
+        # No build step needed
+        dontBuild = true;
+      };
+
+      # --- Build from source ---
+      nargo-from-source = pkgs.rustPlatform.buildRustPackage rec {
+        pname = "nargo";
+        inherit version;
+
+        src = pkgs.fetchFromGitHub {
+          owner = "noir-lang";
+          repo = "noir";
+          rev = "v${version}";
+          hash = "sha256-euY3XJ2jJEnb2XFY7xupKR3OtOepRqIj67X5TzRaJ9k=";
         };
 
-        nargo = rustPlatform.buildRustPackage rec {
-          pname = "nargo";
-          version = "1.0.0-beta.18";
-
-          src = pkgs.fetchFromGitHub {
-            owner = "noir-lang";
-            repo = "noir";
-            rev = "v${version}";
-            hash = "sha256-euY3XJ2jJEnb2XFY7xupKR3OtOepRqIj67X5TzRaJ9k=";
-          };
-
-          cargoLock = {
-            lockFile = "${src}/Cargo.lock";
-            # Automatically fetch any git dependencies referenced in Cargo.lock
-            allowBuiltinFetchGit = true;
-          };
-
-          # Match noirup: RUSTFLAGS="-C target-cpu=native" cargo build --release
-          RUSTFLAGS = "-C target-cpu=native";
-
-          # The noirc_driver build script calls `git rev-parse HEAD`;
-          # in the Nix sandbox there is no .git dir, so provide the value directly.
-          GIT_COMMIT = "v${version}";
-          GIT_DIRTY = "false";
-
-          nativeBuildInputs = [ pkgs.git ];
-
-          # Tests require network access and other resources
-          doCheck = false;
+        cargoLock = {
+          lockFile = "${src}/Cargo.lock";
+          allowBuiltinFetchGit = true;
         };
 
-        lib = pkgs.lib;
-      in
-      {
-        packages = {
-          inherit nargo;
-          default = nargo;
-        };
+        # Same flags as from the `noirup` script
+        RUSTFLAGS = "-C target-cpu=native";
 
-        devShells.default = pkgs.mkShell {
-          buildInputs = [
-            rustToolchain
-            nargo
-          ];
+        # Needed because `noirc_driver` build script requires being in a git repo(?)
+        GIT_COMMIT = "v${version}";
+        GIT_DIRTY = "false";
 
-          NARGO_HOME = builtins.toString ./. + "/.nargo";
+        doCheck = false;
+      };
 
-          shellHook = ''
-            export NARGO_HOME="$NARGO_HOME"
-          '';
-        };
-      }
-    );
+      # --- Switch between the two by changing this line ---
+      nargo = nargo-tarball;
+      # nargo = nargo-from-source;
+    in
+    {
+      packages.${system} = {
+        inherit nargo nargo-tarball nargo-from-source;
+        default = nargo;
+      };
+
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [
+          nargo
+        ];
+
+        # Not sure if this variable is actually needed by `nargo` but doesn't hurt to add it
+        NARGO_HOME = builtins.toString ./. + "/.nargo";
+
+        shellHook = ''
+          export NARGO_HOME="$NARGO_HOME"
+        '';
+      };
+    };
 }
